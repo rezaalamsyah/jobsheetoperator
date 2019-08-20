9a001f854b29d9eb043d9da6eea8551d4ff959bf
rezaalamsyah <53548427+rezaalamsyah@users.noreply.github.com>
2019-08-20 10:58:08 +0700

    first commit

---
+++ src/praktikum1/DataDiri.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktikum1;
+
+/**
+ *
+ * @author santuy
+ */
+public class DataDiri {
+    public static void main(String[] args) {//membuat variabel
+      String nama, alamat, hobi, pekerjaan,motto,TTL,noWA;
+      int usia;
+      double tinggi;
+      char goldar;
+      float bb;
+      
+      //mengisi variable
+      nama ="Santuy";
+      alamat ="Indopipel";
+      usia = 3;
+      tinggi = 123.31;
+      goldar ='A';
+      hobi ="ngops manja";
+      pekerjaan ="sedang menganggur";
+      motto ="santuy enak slur";
+      bb =211930;
+      TTL = "Bogor 8 september 2016";
+      noWA="082244681109";
+      
+      //mencetak ke layar isi variabel
+      System.out.println("Nama: "+ nama);
+      System.out.println("Alamat: "+ alamat);
+      System.out.println("Usia: "+ usia + "tahun");
+      System.out.println("Tinggi: "+ tinggi + "cm");
+      System.out.println("Goldar: "+goldar);
+      System.out.println("Hobi: "+hobi);
+      System.out.println("Pekerjaan: "+pekerjaan);
+      System.out.println("Motto:"+motto);
+      System.out.println("Bb:"+bb+"kg");
+      System.out.println("TTL:"+TTL);
+      System.out.println("noWA:"+noWA);
+      
+    }
+} 
