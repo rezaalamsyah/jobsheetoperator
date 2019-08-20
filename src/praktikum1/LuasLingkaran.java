f5700e36e24b7735f809bf883ba6c7ced55d2537
rezaalamsyah <53548427+rezaalamsyah@users.noreply.github.com>
2019-08-20 11:12:50 +0700

    first commit

--- src/praktikum1/LuasLingkaran.java
+++ src/praktikum1/LuasLingkaran.java
@@ -3,13 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package praktikum1;
 
 /**
  *
  * @author santuy
  */
 public class LuasLingkaran {
-    /**
+      /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
--- src/praktikum1/LuasSegitiga.java
+++ src/praktikum1/LuasSegitiga.java
@@ -1,18 +1,18 @@
-
-import java.util.Scanner;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package praktikum1;
+
+import java.util.Scanner;
 
 /**
  *
  * @author santuy
  */
 public class LuasSegitiga {
-    /**
+      /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
